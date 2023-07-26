34e4550e961c284089e08703d258b03a5f4bd28d
ItaloRochaOliveira <italo.rocha.de.oliveira@gmail.com>
2023-07-26 18:07:45 -0300

    subindo css do index e algumas modificações

--- index.php
+++ index.php
@@ -10,19 +10,19 @@ if(isset($_POST["email"]) && isset($_POST["senha"])){
     $user_query = $mysqli->query("SELECT * FROM clientes WHERE email = '$email'") or die($mysqli->error);
 
    $error = false;
+
+   $error_mensage = false;
     
    if($user_query->num_rows == 0){
-    echo "Email não encontrado.";
+    $error_mensage = "Email não encontrado ou inexistente.";
    }else{
     $user = $user_query->fetch_assoc();
 
     if(!isset($_SESSION))
             session_start();
 
-    var_dump($_SESSION);
-
     if(!password_verify($senha, $user["senha"])){
-        echo "Email ou senha invalidos.";
+        $error_mensage = "Email ou senha invalidos.";
     } else {
         if(!isset($_SESSION))
             session_start();
@@ -46,27 +46,43 @@ if(isset($_POST["email"]) && isset($_POST["senha"])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
+    <link rel="stylesheet" href="css/index.css">
 </head>
 
 <body>
-    <h1>Login</h1>
-    <form action="" method="post">
-        <p>
-            <label for="email">
-                Email:
-            </label>
-            <input type="text" name="email">
-        </p>
+    <div id="container-principal">
+        <div id="cotainer-do-formulario">
+            <h1 id="titulo">Login</h1>
+            <form action="" method="post">
+                <p>
+                    <label for="email" class="label">
+                        Email
+                    </label>
+                    <br>
+                    <input type="text" name="email" value="<?php if(isset($_POST["email"])){echo $_POST["email"];} ?>"
+                        class="input">
+                </p>
 
-        <p>
-            <label for="senha">
-                Senha:
-            </label>
-            <input type="text" name="senha">
-        </p>
+                <p>
+                    <label for="senha" class="label">
+                        Senha
+                    </label>
 
-        <button type="submit">Entrar</button>
-    </form>
+                    <br>
+
+                    <input type="text" name="senha" class="input">
+                </p>
+
+                <?php if(isset($error_mensage)){?>
+                <p id="error-message"><?php echo "*" . $error_mensage?></p>
+                <?php }?>
+
+                <div id="container-login">
+                    <button type="submit" id="login-botao">Entrar</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
 
 </html>
